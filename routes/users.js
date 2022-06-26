7891317aaf715c17249babd584b8e809575c0994
lilmergo <megan.nguyenn261@gmail.com>
2022-06-26 12:49:39 -0600

    Add controllers folder to export functions for routes

--- routes/users.js
+++ routes/users.js
@@ -1,60 +1,19 @@
 import express from 'express';
-import { v4 as uuidv4 } from 'uuid';
 
+
+import { createUser, getAllUsers, getUser, deleteUser, updateUser } from '../controllers/users.js';
 const router = express.Router();
 
 //mock database
-let users = [];
 
-router.get('/', (req, res) => {
-    console.log('users');
-    res.send(users);
+router.get('/', getAllUsers);
 
-})
+router.post('/', createUser);
 
-router.post('/', (req, res) => {
-    //write to database
-    const user = req.body;
+router.get('/:id', getUser);
 
-    users.push({...user, id: uuidv4() });
+router.delete('/:id', deleteUser);
 
-    res.send(`User with the name ${user.firstName} added to the database!`);
-});
-
-router.get('/:id', (req, res) => {
-    const { id } = req.params;
-
-    const foundUser = users.find((user) => user.id == id);
-    res.send(foundUser);
-})
-
-router.delete('/:id', (req, res) => {
-    const { id } = req.params;
-
-    users = users.filter((user) => user.id != id);
-
-    res.send(`User with the id ${id} deleted from the database`);
-});
-
-router.patch('/:id', (req, res) => {
-    const { id } = req.params;
-    const { firstName, lastName, age } = req.body; //possible variables from request body
-    const user = users.find((user) => user.id == id);
-
-    if (firstName) {
-        user.firstName = firstName;
-    }
-
-    if (lastName) {
-        user.lastName = lastName;
-    }
-
-    if (age) {
-        user.age = age;
-    }
-
-    res.send(`User with the id ${id} has been updated`);
-
-})
+router.patch('/:id', updateUser);
 
 export default router;
